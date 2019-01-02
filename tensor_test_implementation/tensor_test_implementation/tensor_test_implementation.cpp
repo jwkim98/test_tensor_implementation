3cfb274fbae9de09d507580e6bd9ddbaccf75848
Apeu <jwkim98@kaist.ac.kr>
2019-01-02 20:17:39 +0900

    TBB set

---
+++ tensor_test_implementation/tensor_test_implementation/tensor_test_implementation.cpp
@@ -0,0 +1,17 @@
+﻿// tensor_test_implementation.cpp : Defines the entry point for the application.
+//
+
+#include "tensor_test_implementation.h"
+#include "tbb/tbb.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	std::cout << "TBB version: " << TBB_VERSION_MAJOR
+		<< "." << TBB_VERSION_MINOR << std::endl;
+	return 0;
+}
